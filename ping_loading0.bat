839d9cae42d8b7c1c29f38937d1283027a9cec3f
priyansh gupta <37746818+priyanshgupta1998@users.noreply.github.com>
2018-11-03 20:53:21 +0530

    Create ping_loading0.bat

---
+++ ping_loading0.bat
@@ -0,0 +1,13 @@
+@echo off
+title Custom-Pause
+echo Loading.
+ping loacalhost -n 2 > nul
+cls
+echo Loading..
+ping loacalhost -n 2 > nul
+cls
+echo Loading...
+ping loacalhost -n 2 > nul
+cls
+echo COMPLETE
+pause
