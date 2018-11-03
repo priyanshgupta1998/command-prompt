c5f09f588c5f44800085dda7b1524d545af3522e
priyansh gupta <37746818+priyanshgupta1998@users.noreply.github.com>
2018-11-03 11:46:56 +0530

    Create hack0.bat

---
+++ hack0.bat
@@ -0,0 +1,11 @@
+@echo off
+color 0a
+mode 120
+echo press Enter to Hack 
+pause >nul
+:a
+netstat -n
+tree
+echo %random%%random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%%random%\
+goto a
