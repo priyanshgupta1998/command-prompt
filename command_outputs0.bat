5e1643ac69a2a702df9390770a2b7eed634e803b
priyansh gupta <37746818+priyanshgupta1998@users.noreply.github.com>
2018-11-03 21:10:08 +0530

    Create command_outputs0.bat

---
+++ command_outputs0.bat
@@ -0,0 +1,66 @@
+C:\Users\PRIYANSH>echo %time%
+21:01:50.77
+
+C:\Users\PRIYANSH>echo %time%
+21:01:53.63
+
+C:\Users\PRIYANSH>echo %time%
+21:01:54.93
+
+C:\Users\PRIYANSH>echo %time%
+21:01:55.69
+
+C:\Users\PRIYANSH>echo %time%
+21:01:56.37
+
+C:\Users\PRIYANSH>echo %date%
+Sat 11/03/2018
+
+C:\Users\PRIYANSH>echo %cd%
+C:\Users\PRIYANSH
+
+C:\Users\PRIYANSH>echo %random%
+4401
+
+C:\Users\PRIYANSH>echo %random%
+6028
+
+C:\Users\PRIYANSH>echo %errorlevel%
+0
+
+C:\Users\PRIYANSH>echo %errorlevel%
+0
+
+C:\Users\PRIYANSH>echo %userprofile%
+C:\Users\PRIYANSH
+
+C:\Users\PRIYANSH>echo %windir%
+C:\Windows
+
+C:\Users\PRIYANSH>echo %systemroot%
+C:\Windows
+
+C:\Users\PRIYANSH>echo %temp%
+C:\Users\PRIYANSH\AppData\Local\Temp
+
+C:\Users\PRIYANSH>echo %python%
+C:\Users\PRIYANSH\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.6
+
+C:\Users\PRIYANSH>echo %opera%
+%opera%
+
+C:\Users\PRIYANSH>echo %opera.exe%
+%opera.exe%
+
+C:\Users\PRIYANSH>echo %notepad%
+%notepad%
+
+C:\Users\PRIYANSH>start firefox.exe
+
+C:\Users\PRIYANSH>
+
+
+
+
+
+
