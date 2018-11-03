cbb94b75b6313e4290f6de84195bfcdc0b573b2f
priyansh gupta <37746818+priyanshgupta1998@users.noreply.github.com>
2018-11-03 11:12:26 +0530

    Add files via upload

---
+++ send.bat
@@ -0,0 +1,39 @@
+@echo off
+color 0c
+title face
+echo face-text
+echo                                  .o        -/          o.                                      
+echo                         `-/osyddmmNNNdd/   `.`.. h--:       /ddNNNmmddyso/-`                        
+echo                     `/sdNMMMdyyhhhNNdhy/o  -ydhyshy-s`     o/yhdNNhhhyydMMMNds/`                    
+echo                  `+hMMMMMNoomMMMMMd`      +mddhsshmMh+          `dMMMMMmooNMMMMMh+`                 
+echo                :yyodMMMNyhdNMMMMMM:      -sNdddyydNNhhs          :MMMMMMNdhyNMMMdoyy:               
+echo              /y++hMMMMdoNMMMMMMMMN`      .ommms /+Nsmmd.         `NMMMMMMMMNodMMMMh++y/             
+echo            :s+sNMMMMMdsyNMMNyNMMMN`       +NhNd-  hN/:dy`        `NMMMNyNMMNysdMMMMMNs+s:           
+echo          `+oyms/sMMMdoMMMd/:hMMMMM:        sddNd/` :ss:s         :MMMMMh:/dMMM+dMMMs/smyo+`         
+echo         -oyy:-yNMMMM/NNy+omMMMMMMMd`       `shhMNs/- .`         `dMMMMMMMmo+yNN/MMMMNy-:yyo-        
+echo        .so./dMMMMMMN+++dMMMMm+dMMMMd.       .+hmmNddys-        .dMMMMmomMMMMd+++NMMMMMMd/.os.       
+echo       /o./mMMMMMMMMN+mMMMMN+:mMMMMMMN+`   `yNNMMMMNmhmNs.    `+NMMMMMMm:+NMMMMm+NMMMMMMMMm+.o/      
+echo     `y+/mMMMy+/mMMMMhydhms.yMMMMMMMMMMd. .sydmdMMMMMMmsNMy. .dMMMMMMMMMMy.smhdyhMMMMm/+yMMMm/+y`    
+echo    .h/yMMNo. /mMMMMMMdh/-.dMd+mMMMMMMMMMNMyMh-+mMdyNMMd-ymNNMMMMMMMMMm+dMd.-/hdMMMMMMd/ .oNMMy/h.   
+echo   .s:mMNo` /mms/::mMMh` .mm/ yMhoMMMMMMmNs.`   :dN+dddd` .sNmMMMMMMohMy /mm. `hMMm::/smm/ `oNMm:s.  
+echo  `/:NMh. /ds-   `sMm/  .mh` /MN-NMhdMMMs/. ../sosMsMMMN`  ./sMMMdhMN-NM/ `hm.  /mMs`   -sd/ .hMN:/` 
+echo  .:NN+ `yo`    /md+`   hMs  oMh.Nm  /mm.  +/hs+shMsddds`    .mm/  mN.hMo  sMh   `+dm/    `oy` +NN:. 
+echo  .NN: -o`   .+yo-     -s.   .m+ -o   `.  /so`./sMhhmdhhh/    .`   o- +m.   .s-     -oy+.   `o- :NN. 
+echo  hN:  -`.::--                `.          -mmhdMNhdmm++MMN+           .`                --::.`-  :Mh 
+echo :Mo  +MNy-                                `:+yyyys:   oMMd-                               -sNM+  oM:
+echo ym` /Mh.                                               dMN.                                 .hM/ `my
+echo No `m+                                       `/hyds+`  yMy.                                   +m` oN
+echo M: /d`                                       hm/``-hm.`md.                                    `d+ :M
+echo m. o-                                       :Ms    `m/ys+                                      -o .m
+echo s- `                                        `dNs-`.:yhh/                                        ` -y
+echo :h`                                           /shddhs:                                           `h:
+echo h                                              .hh:                                              h 
+echo -                                             -d:                                                - 
+echo                                                d.                                                   
+echo                                                d. `-`                                               
+echo                                                -h+s+:+   `.                                         
+echo                                                :/oddNm/-//                                          
+echo                                                 ``-ooo+:`                                           
+echo                             ..``.....`...`..` ```..`  `:`-..-.--.. -..                             
+echo                              ````````` ``````````````   ```` `````` ```     
+pause > nul                       
